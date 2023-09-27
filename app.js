afea6e8d08d4397776b561a9882579345846cecb
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-27 21:48:00 +0200

    handle incoming requests

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const http = require('http');
 const express = require('express');
+//pozwala na konwertowanie body requestu
+const bodyParser = require('body-parser');
 
-//utworzenie aplikacji express
-//czyli pakiet express emituje funkcję, którą możemy
-//przekazać do funkcji crateServer
 const app = express();
 
-const server = http.createServer(app);
+//bodyParser rejestruje middleware który ma w sobie next więc pozwala 
+//na korzystanie z kolejnych middlewarów ale przez niego przechodzi 
+//request co pozwala na parsowanie body. Nie wszystkich rodzajów 
+//oczywiście ale podstawowe ogarnia, dla plików będzie potrzebny inny parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    console.log('In the another middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+});
+
+//zamiast use, który odpala middleware na każdy request ta metoda
+//uruchomi middleware tylko dla metody POST
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    //kolejna fajna funkcja do przekierowywania zamiast 
+    //res.setHeader('Location', '/);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In the another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
