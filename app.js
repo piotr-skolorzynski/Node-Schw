e6aaf155b3c72ec6dd951565a9074b2067daf48a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-27 22:15:17 +0200

    use express router

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 const express = require('express');
-//pozwala na konwertowanie body requestu
 const bodyParser = require('body-parser');
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
 const app = express();
 
-//bodyParser rejestruje middleware który ma w sobie next więc pozwala 
-//na korzystanie z kolejnych middlewarów ale przez niego przechodzi 
-//request co pozwala na parsowanie body. Nie wszystkich rodzajów 
-//oczywiście ale podstawowe ogarnia, dla plików będzie potrzebny inny parser
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/add-product', (req, res, next) => {
-    console.log('In the another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
-});
-
-//zamiast use, który odpala middleware na każdy request ta metoda
-//uruchomi middleware tylko dla metody POST
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    //kolejna fajna funkcja do przekierowywania zamiast 
-    //res.setHeader('Location', '/);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In the another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
+//przekazujemy ścieżki ogarniane przez router w pliku admin
+app.use(adminRouter);
+//przekazujemy ścieżki ogarniane przez router w pliku shop
+app.use(shopRouter);
 
 app.listen(3000);
