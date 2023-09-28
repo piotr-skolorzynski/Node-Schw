f791857fbb01779d22a4b81bd21c40a37853a44f
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-28 08:18:36 +0200

    add not found page

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const shopRouter = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-//przekazujemy ścieżki ogarniane przez router w pliku admin
 app.use(adminRouter);
-//przekazujemy ścieżki ogarniane przez router w pliku shop
 app.use(shopRouter);
+
+//jeżeli żadna ze ścieżek nie pasuje to dojdzie do tego middlware i zwróc page not found
+app.use((req, res, next) => {
+    //ponadto ustaw status odpowiedzi na 404 (not found) - w node jest statusCode
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
