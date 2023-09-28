5eaf0f216ffb0a160a40e2bc6e7cace714a3d49f
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-28 10:27:25 +0200

    serve html pages in routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const adminRouter = require('./routes/admin');
@@ -6,13 +7,11 @@ const shopRouter = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-//można dodać kawałek ścieżki jaki będzie wymagany dla wszystkich w routerze
-// np. /admin/add-product
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,15 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('In the another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    //__dirname zapewnia ścieżkę absolutną do folderu routes (bo w nim są ścieżki)
+    // dlatego po __dirname trzeba dać '../' żeby wyjść wyżej i móc wejść do 
+    //folderu równoległego views
+    //metoda join z node połączy poszczególne argumenty w jedną ścieżkę
+    //zależnie od systemu doda przy każdym / albo dla windows \
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
