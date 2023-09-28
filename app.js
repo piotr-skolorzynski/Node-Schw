5baba13664c80c5cf209a70d7fa2aa52d0ee95a2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-28 12:17:39 +0200

    serve files statically

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+//w ten sposób express umożliwia użytkownikowi bezpośredni dostęp
+//do projektu i folderu public w tym przypadku
+app.use(express.static(path.join('public')));
+
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
